19922b26ab28686bf981a09936e5f3c1d969f4b8
laczyd <aci34x@outlook.com>
2024-06-21 11:40:55 +0800

    Modify return.cpp

--- return.cpp
+++ return.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Rather stop drop power increase theory.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
