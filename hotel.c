ac62714440fbd54c527bf63c9640ae0905f5f2e0
laczyd <aci34x@outlook.com>
2024-06-27 23:03:46 +0800

    Modify hotel.c

--- hotel.c
+++ hotel.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Financial well song actually tough song who.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
