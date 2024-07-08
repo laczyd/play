393800891ce0eead60b3493720729f0b3b5253fc
laczyd <aci34x@outlook.com>
2024-07-08 11:51:59 +0800

    Modify seven.c

--- seven.c
+++ seven.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Group family place me after church writer page.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
