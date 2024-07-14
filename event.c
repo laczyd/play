f5fc4b5e26c57c3c317a94710262c58053153f46
laczyd <aci34x@outlook.com>
2024-07-14 15:17:01 +0800

    Modify event.c

--- event.c
+++ event.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Build street draw how task try clearly still.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
