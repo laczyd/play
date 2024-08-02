a2de4373687dcd783d179b1dc2d7721a53f86424
laczyd <aci34x@outlook.com>
2024-08-02 15:04:55 +0800

    Modify question.cpp

--- question.cpp
+++ question.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Thought choose animal protect value.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
