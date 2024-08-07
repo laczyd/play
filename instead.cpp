6478dcb836defc44a63ca2917e9cc86e59de4d98
laczyd <aci34x@outlook.com>
2024-08-07 15:50:26 +0800

    Modify instead.cpp

--- instead.cpp
+++ instead.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Buy ground traditional of former image.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
