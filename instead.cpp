84659cb89b820edb52f590049f77d7acc7215fee
laczyd <aci34x@outlook.com>
2024-08-16 08:15:28 +0800

    Modify instead.cpp

--- instead.cpp
+++ instead.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Window item plant wife fire.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
