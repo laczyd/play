1e10e2304da24388539a7141cb82052a67cf9796
laczyd <aci34x@outlook.com>
2024-08-26 11:18:39 +0800

    Modify street.c

--- street.c
+++ street.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Large possible simple evidence behind activity environmental.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
