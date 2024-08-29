da15c13d25b957db47978f1c8f2b6900868774ec
laczyd <aci34x@outlook.com>
2024-08-29 14:23:00 +0800

    Modify product.go

--- product.go
+++ product.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Claim indicate people professional husband mother.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
