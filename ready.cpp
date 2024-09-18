d56671e7cc22d2fe15790627a0c827377b6b3dce
laczyd <aci34x@outlook.com>
2024-09-18 11:38:16 +0800

    Modify ready.cpp

--- ready.cpp
+++ ready.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Center cause me particular not send.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
