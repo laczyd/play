1c7fffbda9a5ad5ecd4d68792103f25ed9ba8159
laczyd <aci34x@outlook.com>
2024-10-08 17:39:58 +0800

    Modify instead.cpp

--- instead.cpp
+++ instead.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# But trade team ground law energy sound.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
