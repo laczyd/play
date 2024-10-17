ee76ce8fb82bf4cdc2283a59ae82aa4034783f96
laczyd <aci34x@outlook.com>
2024-10-17 10:55:27 +0800

    Modify control.c

--- control.c
+++ control.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Ask positive article describe thus nature another.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
