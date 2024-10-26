043014b2b78b3cd120221280cd44395d091745ba
laczyd <aci34x@outlook.com>
2024-10-26 13:47:21 +0800

    Modify form.cpp

--- form.cpp
+++ form.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Box company night name officer why fire.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
