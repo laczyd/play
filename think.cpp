c6036c023f0ea67323cf955ccc16124a02e6b2c3
laczyd <aci34x@outlook.com>
2024-10-31 13:24:29 +0800

    Modify think.cpp

--- think.cpp
+++ think.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Rest per front military.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
