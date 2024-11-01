2f4ce45a3de181b8d47286bf3de1a3e73831cb72
laczyd <aci34x@outlook.com>
2024-11-01 12:59:42 +0800

    Modify form.cpp

--- form.cpp
+++ form.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Bag tend take trial success likely example bar.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
