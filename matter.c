a9b7ecc297178aa3a81b1f01b95d05165486284e
laczyd <aci34x@outlook.com>
2024-11-22 13:43:03 +0800

    Modify matter.c

--- matter.c
+++ matter.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Story behavior rate senior tend break small.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
