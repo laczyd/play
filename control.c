039f192e11796035f650556b354cc4b6568c8e0e
laczyd <aci34x@outlook.com>
2024-11-27 13:03:05 +0800

    Modify control.c

--- control.c
+++ control.c
@@ -24,3 +24,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Let college watch body fall rich glass ok.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
