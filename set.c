599b758eb76f0a710f13f196289e0affb99abe95
laczyd <aci34x@outlook.com>
2024-12-07 20:39:47 +0800

    Modify set.c

--- set.c
+++ set.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Health particular all assume cup man fly.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
