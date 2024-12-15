4579098f9b5d284770e9b2aa2da4e53d888bb5d1
laczyd <aci34x@outlook.com>
2024-12-15 19:18:27 +0800

    Modify mother.c

--- mother.c
+++ mother.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Style manage culture out charge south sell.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
