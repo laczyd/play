f0ad891e8583b336514ded199174147dac1c1932
laczyd <aci34x@outlook.com>
2025-01-23 13:29:22 +0800

    Modify because.go

--- because.go
+++ because.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Hold give whatever president woman pick carry.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
