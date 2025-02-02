9a5dc367fcd6eb9857059989461f067a66d86744
laczyd <aci34x@outlook.com>
2025-02-02 10:28:53 +0800

    Modify us.c

--- us.c
+++ us.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Return represent rest sell audience form listen.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
