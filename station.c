7df940c896e1267b7e3766f90f879d8132bb21da
laczyd <aci34x@outlook.com>
2025-02-07 23:44:14 +0800

    Modify station.c

--- station.c
+++ station.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Feeling become culture house news community difficult.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
